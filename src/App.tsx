fd924e98796cd5b331faed6f13b501057cb643a9
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2026-02-24 09:24:09 -0300

    feat: add new lang, title and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
-}
+import { AppRoutes } from "./routes";
 
-export default App;
+export default function App() {
+  return <AppRoutes />;
+}
