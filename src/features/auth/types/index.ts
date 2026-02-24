9456ce719aba004e731384ee65416661d59ba602
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2026-02-24 09:28:48 -0300

    feat(auth): create types for login, register and user

---
+++ src/features/auth/types/index.ts
@@ -0,0 +1,25 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  token: string;
+  user: User;
+}
+
+export interface RegisterRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface RegisterResponse {
+  userId: string;
+}
